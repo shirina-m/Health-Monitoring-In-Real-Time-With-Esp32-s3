4585555ffaaa58a0c4051ec0f5d1baba473db0c5
Sultan Khaled <113555443+ReyoZakie@users.noreply.github.com>
2025-07-16 20:30:52 +0300

    Delete secrets.h

--- secrets.h
+++
@@ -1,4 +0,0 @@
-//secrets.h
-
-#define API_KEY         "AIzaSyDBv9JjYCuBsEs-Y42lQvEfI3c2W0evuM8"
-#define DATABASE_URL    "https://healthmonitoring-21f90-default-rtdb.europe-west1.firebasedatabase.app/"

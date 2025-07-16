47ab1ea87c0eaa86850894ac131a9a248f11648d
Sultan Khaled <113555443+ReyoZakie@users.noreply.github.com>
2025-07-16 20:25:56 +0300

    Create secrets.h

---
+++ secrets.h
@@ -0,0 +1,4 @@
+//secrets.h
+
+#define API_KEY         "AIzaSyDBv9JjYCuBsEs-Y42lQvEfI3c2W0evuM8"
+#define DATABASE_URL    "https://healthmonitoring-21f90-default-rtdb.europe-west1.firebasedatabase.app/"
